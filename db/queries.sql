1bd96a0d6cc26ab060dcf9e30b7e897cfc851282
Wendel Conceição Souza <77546408+cswendel@users.noreply.github.com>
2026-04-16 20:07:55 -0300

    Update queries.sql

--- db/queries.sql
+++ db/queries.sql
@@ -15,12 +15,5 @@ JOIN movies m ON r.movie_id = m.movie_id
 CROSS JOIN LATERAL unnest(string_to_array(m.genre, ',')) AS genre_name
 GROUP BY genero
 ORDER BY media_avaliacao DESC
-LIMIT 5;
+LIMIT 1;
 
--- 3. Usuário que mais assiste filmes
-SELECT u.name, COUNT(r.user_id) AS filmes_assistidos
-FROM ratings r
-JOIN users u ON r.user_id = u.user_id
-GROUP BY u.name
-ORDER BY filmes_assistidos DESC
-LIMIT 5;
